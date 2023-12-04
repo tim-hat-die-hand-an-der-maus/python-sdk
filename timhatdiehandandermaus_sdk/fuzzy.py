781c2e169f345c04bd231ada915a0be9556d1def
Torben Carstens <git@carstens.tech>
2023-12-04 21:38:21 +0100

    Formatting

--- timhatdiehandandermaus_sdk/fuzzy.py
+++ timhatdiehandandermaus_sdk/fuzzy.py
@@ -3,7 +3,9 @@ from thefuzz import fuzz
 from timhatdiehandandermaus_sdk import models
 
 
-def fuzzy_search_movie(movies: list[models.MovieResponse], title: str, *, threshold: int):
+def fuzzy_search_movie(
+    movies: list[models.MovieResponse], title: str, *, threshold: int
+):
     matches = []
     for movie in movies:
         if (
